f5403ae2ce682711b5fbafd72dda11f64820850f
Irina Baldwin <2007inb@gmail.com>
2018-09-18 14:37:47 +0100

    Add print by cohort

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -11,20 +13,32 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november},
 ]
+
+$cohorts = Set[:january, :february, :march, :april, :may, :june, :july,
+              :august, :september, :october, :november, :december]
+
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please enter the name and cohort of each student"
   puts "To finish, just hit return twice"
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  user_input = gets.chomp.split(/[\s,]+/)
+  name = user_input[0]
+  cohort = user_input[1]
+  if user_input[1] != nil
+    cohort = user_input[1].to_sym
+  else cohort = :november
+  end # there is def a more graceful way to do this OR assignment
   # while the name is not empty, repeat this code
-  while !name.empty? do
+  while name != nil do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    user_input = gets.chomp.split(/[\s,]+/) #this code is duplicated!
+    name = user_input[0]
+    cohort = user_input[1].to_sym if user_input[1] != nil
   end
   # return the array of students
   students
@@ -35,11 +49,20 @@ def print_header
   puts "---------------".center($margin)
 end
 def print(students)
-  students.each_with_index do |student, index|
-    if student[:name].length < 12
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center($margin)
-    end
+  students_by_cohort = {}
+  students.map do |student|
+    students_by_cohort[student[:cohort]] ||= []
+    students_by_cohort[student[:cohort]] << student[:name]
   end
+  students_by_cohort.each do |key, value|
+    puts "#{key}:"
+    value.each {|name| puts "#{name}"}
+  end
+  # students.each_with_index do |student, index|
+  #   if student[:name].length < 12
+  #     puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center($margin)
+  #   end
+  # end
 end
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
