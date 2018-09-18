10dfa4e50992c5a2a606ad044e9193218095bc51
Irina Baldwin <2007inb@gmail.com>
2018-09-18 14:40:42 +0100

    Change to 'now we have 1 student'

--- directory.rb
+++ directory.rb
@@ -34,7 +34,11 @@ def input_students
   while name != nil do
     # add the student hash to the array
     students << {name: name, cohort: cohort}
-    puts "Now we have #{students.count} students"
+    if students.length == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end
     # get another name from the user
     user_input = gets.chomp.split(/[\s,]+/) #this code is duplicated!
     name = user_input[0]
